234024e90925c28e2e8cb2ab0242b26f85f5f62a
annazetterstrom <anna.wiktoria@me.com>
2019-12-17 14:05:57 +0100

    text uppdates

--- pages/Risingstars.vue
+++ pages/Risingstars.vue
@@ -29,9 +29,9 @@
           >
             <p class="black--text">
               Som ny inom konsultbranschen har jag mycket att lära mig och jag
-              tror att detta program kommer ge mig rätt verktyg för att starta
-              min resa på B3. Jag ser fram emot att få en mentor som kan hjälpa
-              och stötta mig på vägen. Utöver det tror jag att kunskapsutbytet
+              tror att detta program kan ge mig rätt verktyg för att starta
+              min resa på B3. Jag gillar att man dessutom får en mentor som kan hjälp
+              och stötta på vägen. Utöver det verkar kunskapsutbytet
               mellan deltagarna i programmet kommer vara otroligt lärorikt och
               jag ser fram emot att få möjlighet att skapa ett större kontaktnät
               inom företaget.
